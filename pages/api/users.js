75a3fef1285de3aa586395bd0dcd20dd0927e77a
irham nurullah <nurullahirham@gmail.com>
2024-02-01 22:00:09 +0700

    remove prisma

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,25 +1,26 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import mysql from 'mysql2';
 
-export default async function handlerAddUser(req, res) {
-  console.log(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-  if (req.method !== 'POST') return res.status(405).json({ message: process.env.DATABASE_URL });
+export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).json({ message: 'Method Not Allowed' });
+  const data = req.body;
 
   try {
-    const { name, email, company_name, industry } = req.body;
+    const query = 'INSERT INTO m_users (name, email, company_name, industry) VALUES (?, ?, ?, ?)';
+    const [result] = await pool.promise().query(query, [data.name, data.email, data.company_name, data.industry]);
 
-    const user = await prisma.m_users.create({
-      data: {
-        name,
-        email,
-        company_name,
-        industry,
-      },
-    });
-    console.log(user);
-    res.status(201).json({ status: 'Success', message: 'Berhasil menambah data' });
+    res.status(201).json({ message: 'Data berhasil disimpan ke database' });
   } catch (error) {
-    res.status(400).json(error);
+    console.error('Gagal menyimpan data ke database: ', error);
+    res.status(500).json({ message: 'Gagal menyimpan data ke database' });
   }
 }
